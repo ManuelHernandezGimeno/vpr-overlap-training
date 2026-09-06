df31cf7ca1dd8f19c1320c56e3c550848c0584c7
ManuelHernandezGimeno <manuher1224@gmail.com>
2026-09-07 01:39:51 +0200

    Update enroot.sh

--- cluster/container/enroot.sh
+++ cluster/container/enroot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Run this from /raid/ropert/mhernang/VPR/docker
-rm -f vpr_train_mhernang+latest.sqsh
+# Run this from REPO_ROOT/workspace/vpr-overlap-training/cluster/container
+rm -f vpr-overlap-training+latest.sqsh
 
-enroot import dockerd://vpr_train_mhernang:latest
+enroot import dockerd://vpr-overlap-training:latest
