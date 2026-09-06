f86ac06553a214ee3b09807a8f41cfcda6791c47
ManuelHernandezGimeno <manuher1224@gmail.com>
2026-09-07 01:46:03 +0200

    Update run.sh

--- cluster/container/run.sh
+++ cluster/container/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-USER_VPR_ROOT=/raid/ropert/mhernang/VPR
+USER_VPR_ROOT=REPO_ROOT
 DATA_VPR_LINK=${USER_VPR_ROOT}/data_vpr
 MAPILLARY_ROOT=$(readlink -f ${DATA_VPR_LINK}/mapillary)
 
 # Optional interactive test. For real training, use SLURM with ../train.sh.
 docker run --gpus all --ipc=host -it \
-  -v ${USER_VPR_ROOT}:/workspace/mhernang/VPR \
-  -v ${MAPILLARY_ROOT}:/workspace/mhernang/VPR/data/mapillary \
-  -w /workspace/mhernang/VPR \
-  vpr_train_mhernang:latest bash
+  -v ${USER_VPR_ROOT}:/workspace/vpr-overlap-training \
+  -v ${MAPILLARY_ROOT}:/workspace/vpr-overlap-training/data/mapillary \
+  -w /workspace/vpr-overlap-training \
+  vpr-overlap-training:latest bash
